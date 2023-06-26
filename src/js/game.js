3296533ee4b38cc74fcc0e765e73149a67422f6d
vymat <vymatvienko@gmail.com>
2023-06-26 15:52:48 +0300

    Задание выполнено

---
+++ src/js/game.js
@@ -0,0 +1,19 @@
+import Character from "./domain";
+
+export default class Game {
+    start() {
+      console.log('game started');
+    }
+  }
+  
+  export class GameSavingData {
+
+  }
+  
+  export function readGameSaving() {
+
+  }
+  
+  export function writeGameSaving() {
+
+  }
